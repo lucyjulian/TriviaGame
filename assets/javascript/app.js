bc8a51d846b35cc6279cb1805975cdff2fc64d5a
Lucy Julian <lucyjulian@Lucys-MacBook-Pro.local>
2018-03-26 15:44:05 -0400

    working and frog background on

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,64 @@
 // V A R I A B L E S //
+var intervalId;
+var clockRunning = false;
+var stopwatch = {
+
+    time: 30,
+  
+    reset: function() {
+  
+      stopwatch.time = 30;
+      
+      $("#display").text("00:30");
+
+    },
+    start: function() {
+  
+      // DONE: Use setInterval to start the count here and set the clock to running.
+      if (!clockRunning) {
+        intervalId = setInterval(stopwatch.count, 1000);
+        clockRunning = true;
+      }
+    },
+    stop: function() {
+  
+      // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+      clearInterval(intervalId);
+      clockRunning = false;
+    },
+    count: function() {
+  
+      stopwatch.time--;
+
+      var converted = stopwatch.timeConverter(stopwatch.time);
+      console.log(converted);
+  
+      $("#display").text(converted);
+
+      if (stopwatch.time === 0){
+        outOfTime();
+        }
+    },
+    timeConverter: function(t) {
+  
+      var minutes = Math.floor(t / 60);
+      var seconds = t - (minutes * 60);
+  
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+  
+      if (minutes === 0) {
+        minutes = "00";
+      }
+      else if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+  
+      return minutes + ":" + seconds;
+    }
+  };
+
 var allQuestions = {
     1: {
         question: "On how many continents are frogs found?",
@@ -19,11 +79,17 @@ var allQuestions = {
         question: "How do frogs drink water?",
         choices: ["They just absorb it through their skin", "Through their nostrils, kind of like the blowhole of a whale", "They lap it up with their tongue like a dog", "They just swallow it while they are swimming"],
         answer: "They just absorb it through their skin"
+    },
+    5: {
+        question: "How do poison dart frogs get their poison?",
+        choices: ["It is synthesized in their skin","It is synthesized in a gland in their neck", "They are born with a fixed reserve", "From eating poisonous bugs"],
+        answer: "From eating poisonous bugs"
     }
 };
 var currentQuestionPosition = 0;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
+var youRanOutOfTime = 0;
 
 
 // F U N C T I O N S //
@@ -33,7 +99,7 @@ function endOfGame(){
     $("#question").html("Hey! You made it to the end of the game...");
     $("#possibleAnswer1").html("You answered this many questions right..." + correctAnswers);
     $("#possibleAnswer2").html("and this many questions wrong..." + incorrectAnswers);
-    $("#possibleAnswer3").html("Do you wanna play again?");
+    $("#possibleAnswer3").html("you ran out of time this many times..." + youRanOutOfTime);
     $("#possibleAnswer4").html("click here to hop back to the beginning of the game!");
     $("#possibleAnswer4").on("click", function(){
         currentQuestionPosition = 0;
@@ -43,8 +109,20 @@ function endOfGame(){
         checkAnswer();
     })
 }
+function outOfTime() {
+    youRanOutOfTime++;
+    stopwatch.stop();
+    $("#question").html("OH no! You ran out of time!");
+    $("#possibleAnswer1").html("The correct answer");
+    $("#possibleAnswer2").html("was");
+    $("#possibleAnswer3").html(allQuestions[currentQuestionPosition].answer);
+    $("#possibleAnswer4").html("!!!!!!!!");
+    setTimeout(displayQuestion, 4000);
+}
 function displayQuestion(){
-    if (currentQuestionPosition < 4){
+    if (currentQuestionPosition < 5){
+        stopwatch.reset();
+        stopwatch.start();
         currentQuestionPosition++;
         $("#question").html(allQuestions[currentQuestionPosition].question);
         $("#possibleAnswer1").html(allQuestions[currentQuestionPosition].choices[0]);
@@ -56,6 +134,7 @@ function displayQuestion(){
     }
 };
 function wrongAnswer(){
+    stopwatch.stop();
     incorrectAnswers++;
     $("#question").html("you got it WRONG");
     $("#possibleAnswer1").html("The correct answer");
@@ -65,6 +144,7 @@ function wrongAnswer(){
     setTimeout(displayQuestion, 4000);
 }
 function rightAnswer(){
+    stopwatch.stop();
     correctAnswers++;
     $("#question").html("you got it RIGHT");
     $("#possibleAnswer1").html("Good job!");
@@ -111,7 +191,6 @@ function checkAnswer(){
         }
     });
 }
-
 
 
 // A C T I O N S //
